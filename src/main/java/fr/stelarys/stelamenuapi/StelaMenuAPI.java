602dcfb54efeee8916053e97e0ed60a71a804785
Plhume <felix@exodius.fr>
2022-09-13 21:11:53 +0200

    Update StelaMenuAPI.java

--- src/main/java/fr/stelarys/stelamenuapi/StelaMenuAPI.java
+++ src/main/java/fr/stelarys/stelamenuapi/StelaMenuAPI.java
@@ -1,8 +1,14 @@
 package fr.stelarys.stelamenuapi;
 
+import fr.stelarys.stelamenuapi.managers.MenuManager;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.HashMap;
+
 public final class StelaMenuAPI extends JavaPlugin {
+
+    private static final HashMap<Player, MenuManager> menuMap = new HashMap<>();
 
     @Override
     public void onEnable() {
@@ -12,5 +18,18 @@ public final class StelaMenuAPI extends JavaPlugin {
     @Override
     public void onDisable() {
         getLogger().info("Plugin disabled!");
+    }
+
+    public static MenuManager getMenuManager(Player player) {
+        MenuManager menuManager;
+
+        if (!(menuMap.containsKey(player))) {
+            menuManager = new MenuManager(player);
+            menuMap.put(player, menuManager);
+
+            return menuManager;
+        } else {
+            return menuMap.get(player);
+        }
     }
 }
